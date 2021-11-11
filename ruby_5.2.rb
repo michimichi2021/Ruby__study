c75aeb7db508775d805176eab95baa4004a8e282
michimichi2021 <michikaho1009@gmail.com>
2021-11-11 23:56:15 +0900

    5.2.rb

---
+++ ruby_5.2.rb
@@ -0,0 +1,7 @@
+puts {}.class
+currencies={'japan'=>'yen','us'=>'dollar','india'=>'rupee'}
+currencies['Italy']='euro'
+puts currencies
+currencies['japan']='円'
+puts currencies
+puts currencies['brazil']
