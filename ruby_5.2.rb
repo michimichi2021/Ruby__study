1bd6dea096964e9370055062aa608e8345f5d335
michimichi2021 <michikaho1009@gmail.com>
2021-11-13 23:41:43 +0900

    ruby.5.2.2.rb

--- ruby_5.2.rb
+++ ruby_5.2.rb
@@ -5,3 +5,22 @@ puts currencies
 currencies['japan']='円'
 puts currencies
 puts currencies['brazil']
+
+#5.2.2.rb
+currencies.each do |key,value|
+  puts "#{key}:#{value}"
+end
+
+currencies.each do |key_value|
+  key2 = key_value[0]
+  value2 = key_value[1]
+  puts "#{key2}:#{value2}"
+end
+
+
+a={'x'=>1,'y'=>2,'z'=>3}
+b={'x'=>1,'y'=>2,'z'=>3}
+c={'x'=>10,'y'=>2,'z'=>3}
+puts a==b
+puts a==c
+puts a.size
