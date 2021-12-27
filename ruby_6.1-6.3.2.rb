bad0208fd98dd1671fea38d8e5e6b373cbfcd5f5
michimichi2021 <michikaho1009@gmail.com>
2021-12-27 23:55:48 +0900

    6.3終了

--- ruby_6.1-6.3.2.rb
+++ ruby_6.1-6.3.2.rb
@@ -39,5 +39,38 @@ word = '私の誕生日は1977年7月17日です。'
 m = /(\d+)年(\d+)月(\d+)日/.match(word)
 puts m[1]
 puts m[2]
+puts m[2,2]
 puts m[3]
+puts m[-1]
+puts m[1..3]
 puts /(\d+)年(\d+)月(\d+)日/.match(word)
+
+n = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(word)
+puts n[:year]
+puts n[:month]
+puts n[:day]
+puts n['year']
+puts n[2]
+
+#正規表現は左辺に来なければいけない、正規オブジェクトを変数に入れない
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ word
+  puts "#{year}/#{month}/#{day}"
+end
+
+#正規表現と組み合わせると便利なStringクラスのメソッド
+p '123 456 789'.scan(/\d+/)
+p '1977年7月17日2016年12月31日'.scan(/(\d+)年(\d+)月(\d+)日/)
+#キャプチャはしないが、グループ化はする
+p '1977年7月17日2016年12月31日'.scan(/(?:\d+)年(?:\d+)月(?:\d+)日/)
+post = '郵便番号は123-4567です'
+puts post[/\d{3}-\d{4}/]
+puts word[/(\d+)年(\d+)月(\d+)日/,3]
+puts post.slice(/\d{3}-\d{4}/)
+puts word.slice(/(\d+)年(\d+)月(\d+)日/,3)
+puts post.slice!(/\d{3}-\d{4}/)
+p post
+number = '123,456-789'
+p number.split(',')
+number2 = '123,456-789'
+puts number2.gsub(',',':')
+puts word.gsub(/(\d+)年(\d+)月(\d+)日/,'\1-\2-\3')
