c382b62cadd4753cc26132389595934a5e4a9e40
michimichi2021 <michikaho1009@gmail.com>
2021-12-29 22:39:21 +0900

    6.4途中

---
+++ ruby_book/test/convert_hash_syntax_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../lib/convert_hash_syntax'
+
+class ConvertHashSyntaxTest < Minitest::Test
+  def test_convert_hash_syntax
+   old_syntax = <<~TEST
+
+   TEXT
+  end
+end
