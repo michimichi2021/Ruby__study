b181b0ebec8769a947cacb0c3c73ba775283aa74
michimichi2021 <michikaho1009@gmail.com>
2022-01-05 02:14:33 +0900

    7章途中

--- ruby_7.0.rb
+++ ruby_7.0.rb
@@ -1,13 +1,30 @@
-users = []
-users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
-users << { first_name: 'Bob', last_name: 'Python', age: 30 }
-
-
-
-def full_name(user)
-  "#{user[:first_name]}#{user[:last_name]}"
+class User
+  attr_reader :first_name, :last_name, :age
+  def initialize(first_name,last_name,age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+  def full_name
+    "#{first_name}#{last_name}"
+  end
 end
+
+
+users = []
+users << User.new('Alice', 'Ruby', 20 )
+users << User.new('Bob', 'Python', 30 )
+
+
 
 users.each do |user|
-  puts "氏名:#{full_name(user)}、年齢:#{user[:age]}"
+  puts "氏名:#{user.full_name}、年齢:#{user.age}"
 end
+
+# puts users[0][:first_name]
+# puts users[1][:first_name]
+
+# users[0][:country] = 'japan'
+# users[0][:first_name] = 'Carol'
+
+# puts users[0]
